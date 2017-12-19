8b9fa5f542d8defa76c365ae5f0fb666c71cd272
Danillo Barreto <danillosbarreto@gmail.com>
2017-12-19 17:16:33 -0200

    Aplicando herança

--- models.py
+++ models.py
@@ -7,6 +7,23 @@ class Perfil(object):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
+		self.__curtidas = 0
 
 	def imprimir(self):
 		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+
+	def curtir(self):
+		self.__curtidas+=1
+
+	def obter_curtidas(self):
+		return self.__curtidas
+
+class PerfilVip(Perfil):
+	'Classe padrão para perfis de usuários vips'
+
+	def __init__(self, nome, telefone, empresa, apelido):
+		super(PerfilVip, self).__init__(nome, telefone, empresa)
+		self.apelido = apelido
+
+	def obter_creditos(self):
+		return super(PerfilVip, self).obter_curtidas() * 10.0
